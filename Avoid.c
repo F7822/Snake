85c5377827a02cec2d58b0a3dd8c81bf354fe49c
fjl <fanjl22@mails.tsinghua.edu.cn>
2024-07-12 16:48:33 +0800

    Add P&R and obstacle avoidance

---
+++ Avoid.c
@@ -0,0 +1,13 @@
+/*
+加速运动？
+
+
+*/
+#include "Avoid.h"
+
+//在0.5m内没有障碍物
+void Go_Straight(){
+    
+}
+void Turn_Right();
+void Turn_Left();
---
+++ Avoid.h
@@ -0,0 +1,8 @@
+#ifndef AVOID_H
+#define Avoid_h
+
+void Go_Straight();
+void Turn_Right();
+void Turn_Left();
+
+#endif
